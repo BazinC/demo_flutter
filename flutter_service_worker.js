9d517e4a05ba146da10e9d7b3d66709409b6bc68
BazinC <BazinC@users.noreply.github.com>
2020-09-11 05:21:56 +0000

    deploy: 9390e2c1ff3f402c3d2b30e8ad5508c55be67f19

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,12 +3,15 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "main.dart.js": "6c980cc34bc7f5a7fdf00a6684f287fe",
-"assets/NOTICES": "5bc6ec2d1d495b2ca82fda9bc68d5b0c",
-"assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
+  "main.dart.js": "4b9df5c3c35b3eb7082fc3d8e64cf51f",
+"assets/NOTICES": "edfcee1fc01c745ce14d1fe17f629bfe",
+"assets/FontManifest.json": "5a32d4310a6f5d9a6b651e75ba0d7372",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "2aa350bd2aeab88b601a593f793734c0",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "5a37ae808cf9f652198acde612b5328d",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "2bca5ec802e40d3f4b60343e346cedde",
 "assets/fonts/MaterialIcons-Regular.otf": "a68d2a28c526b3b070aefca4bac93d25",
-"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+"assets/AssetManifest.json": "801125fca546f506babac9af4ec061ba",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "index.html": "2a5be9730c00d7b1ba34559eb14b3c04",
 "/": "2a5be9730c00d7b1ba34559eb14b3c04",
